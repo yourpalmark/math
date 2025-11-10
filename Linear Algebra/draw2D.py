7be44be335ef56ada2c7ee6bd8f69732ee7b8437
Mark Walters <mark@yourpalmark.com>
2025-11-10 01:29:57 -0600

    Added initial Linear Algebra folder with vector and draw modules and Chapter 0-2 notebooks

---
+++ Linear Algebra/draw2D.py
@@ -0,0 +1,216 @@
+from math import ceil, floor, sqrt
+
+import numpy as np
+
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import xlim, ylim
+
+__all__ = ["Point2D", "Points2D", "Line2D", "Arrow2D", "draw2D"]
+
+class Point2D():
+    def __init__(self, x, y, color='black', label=None):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.label = label
+
+class Points2D():
+    def __init__(self, *points, color='black'):
+        length = len(points)
+        points = list(map(process_pt, points, [color]*length))
+        self.points = list(points)
+        self.color = color
+
+class Line2D():
+    def __init__(self, start_point, end_point, color='gray', linestyle='solid', label=None):
+        self.start_point = start_point
+        self.end_point = end_point
+        self.color = color
+        self.linestyle = linestyle
+        self.label = label
+
+class Arrow2D():
+    def __init__(self, head, tail=(0,0), color='red', linestyle='solid', label=None):
+        self.head = head
+        self.tail = tail
+        self.color = color
+        self.linestyle = linestyle
+        self.label = label
+
+def process_pt(pt, color):
+    length = len(pt)
+    x, y, col, label = 0, 0, color, None
+    if length > 0:
+        x = pt[0]
+        if length > 1:
+            y = pt[1]
+            if length > 2:
+                col = pt[2]
+                if length > 3:
+                    label = pt[3]
+    return Point2D(x, y, color=col, label=label)
+    
+def extract_vectors_2D(objects):
+    for obj in objects:
+        if type(obj) == Point2D:
+            yield (obj.x, obj.y)
+        elif type(obj) == Points2D:
+            for p in obj.points:
+                yield (p.x, p.y)
+        elif type(obj) == Line2D:
+            yield obj.start_point
+            yield obj.end_point
+        elif type(obj) == Arrow2D:
+            yield obj.head
+            yield obj.tail
+        else:
+            raise TypeError("Unrecognized object: {}".format(obj))
+
+def get_label_xy(start_point, end_point=None):
+    x1, y1 = start_point
+    
+    if end_point is not None:
+        x2, y2 = end_point
+        mx, my = (x1+x2)/2, (y1+y2)/2
+        vx, vy = (x2-x1), (y2-y1)
+    else:
+        mx, my = x1, y1
+        vx, vy = x1, y1
+
+    # perpendicular offset by delta
+    norm = (vx**2 + vy**2)**0.5 or 1.0
+    nx, ny = -vy/norm, vx/norm
+    delta = 0.2  # data-units
+    
+    if end_point is not None:
+        lx, ly = mx + delta*nx, my + delta*ny
+    else:
+        lx, ly = x1, my + delta*ny
+    
+    yield lx
+    yield ly
+
+def draw2D(*objects, origin=False, axes=True, axes_labels=False, ticks=True, tick_labels=True, grid=False, grid_size=(1,1), dark_mode=True, width=6, dpi=100, nice_aspect_ratio=True, save_as=None):
+
+    fig = plt.gcf()
+    
+    if dark_mode:
+        plt.style.use('dark_background')
+        axes_color = 'white'
+    else:
+        plt.style.use('default')
+        axes_color = 'black'
+    
+    all_vectors = list(extract_vectors_2D(objects))
+    all_x, all_y = zip(*all_vectors)
+
+    max_x, min_x = max(0,*all_x), min(0,*all_x)
+    max_y, min_y = max(0,*all_y), min(0,*all_y)
+
+    x_size = max_x-min_x
+    y_size = max_y-min_y
+    
+    # Grid
+    x_padding = max(ceil(0.02 * x_size), grid_size[0])  # Reduced padding from 0.05 to 0.02
+    y_padding = max(ceil(0.02 * y_size), grid_size[1])  # Reduced padding from 0.05 to 0.02
+
+    def round_up_to_multiple(val,size):
+        return floor((val + size) / size) * size
+
+    def round_down_to_multiple(val,size):
+        return -floor((-val - size) / size) * size
+
+    plt.xlim(floor((min_x - x_padding) / grid_size[0]) * grid_size[0],
+            ceil((max_x + x_padding) / grid_size[0]) * grid_size[0])
+    plt.ylim(floor((min_y - y_padding) / grid_size[1]) * grid_size[1],
+            ceil((max_y + y_padding) / grid_size[1]) * grid_size[1])
+
+    if origin:
+        plt.scatter([0],[0], color=axes_color, marker='x', zorder=3)
+
+    ax = plt.gca()
+    
+    if not tick_labels:
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+    
+    if not ticks:
+        ax.set_xticks([])
+        ax.set_yticks([])
+    else:
+        ax.set_xticks(np.arange(plt.xlim()[0],plt.xlim()[1],grid_size[0]))
+        ax.set_yticks(np.arange(plt.ylim()[0],plt.ylim()[1],grid_size[1])) 
+    
+    if grid:
+        plt.grid(True, alpha=0.6)
+        
+    ax.set_axisbelow(True)
+
+    if axes_labels:
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+    
+    if axes:
+        ax.axhline(linewidth=2, color=axes_color, zorder=1)
+        ax.axvline(linewidth=2, color=axes_color, zorder=1)
+
+    # Objects
+    for obj in objects:
+        if type(obj) == Point2D:
+            plt.scatter(obj.x, obj.y, color=obj.color, zorder=4)
+            if obj.label is not None:
+                lx, ly = get_label_xy((obj.x, obj.y))
+                plt.annotate(obj.label, (lx, ly), ha='center')
+        elif type(obj) == Points2D:
+            all_x = [p.x for p in obj.points]
+            all_y = [p.y for p in obj.points]
+            all_colors = [p.color for p in obj.points]
+            all_labels = [p.label for p in obj.points]
+            plt.scatter(all_x, all_y, color=all_colors, zorder=4)
+            for i, txt in enumerate(all_labels):
+                if txt is not None:
+                    lx, ly = get_label_xy((all_x[i], all_y[i]))
+                    plt.annotate(txt, (lx, ly), ha='center')
+        elif type(obj) == Line2D:
+            x1, y1 = obj.start_point
+            x2, y2 = obj.end_point
+            plt.plot([x1,x2],[y1,y2], color=obj.color, linestyle=obj.linestyle, zorder=2)
+            if obj.label is not None:
+                lx, ly = get_label_xy(obj.start_point, obj.end_point)
+                plt.annotate(obj.label, xy=(lx, ly), xytext=(lx, ly), ha='center')
+        elif type(obj) == Arrow2D:
+            head, tail = obj.head, obj.tail
+            head_length = (xlim()[1] - xlim()[0]) / 20.
+            length = sqrt((head[0]-tail[0])**2 + (head[1]-tail[1])**2)
+            new_length = length - head_length
+            new_y = (head[1] - tail[1]) * (new_length / length)
+            new_x = (head[0] - tail[0]) * (new_length / length)
+            ax.arrow(tail[0], tail[1], new_x, new_y,
+                head_width=head_length/1.5, head_length=head_length,
+                fc=obj.color, ec=obj.color, ls=obj.linestyle, zorder=3)
+            if obj.label is not None:
+                lx, ly = get_label_xy(tail, head)
+                ax.annotate(obj.label, xy=(lx, ly), xytext=(lx, ly), ha='center')
+        else:
+            raise TypeError("Unrecognized object: {}".format(obj))
+    
+    # Size
+    current_size = fig.get_size_inches()
+    
+    if nice_aspect_ratio:
+        coords_height = (ylim()[1] - ylim()[0])
+        coords_width = (xlim()[1] - xlim()[0])
+        fig.set_size_inches(width, width * coords_height / coords_width)
+        fig.set_dpi(dpi)
+    else:
+        ratio = current_size[0]/width
+        fig.set_size_inches(width, current_size[1]/ratio)
+        fig.set_dpi(dpi)
+
+    if save_as:
+        plt.savefig(save_as, dpi=fig.dpi)
+    
+    plt.show()
+
+
---
+++ Linear Algebra/draw3D.py
@@ -0,0 +1,247 @@
+from math import sqrt
+
+import numpy as np
+
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
+from mpl_toolkits.mplot3d import Axes3D, proj3d
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+__all__ = ["Point3D", "Points3D", "Line3D", "Arrow3D", "draw3D"]
+
+class Point3D():
+    def __init__(self, x, y, z, color='black', label=None):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.color = color
+        self.label = label
+
+class Points3D():
+    def __init__(self, *points, color='black'):
+        length = len(points)
+        points = list(map(process_pt, points, [color]*length))
+        self.points = list(points)
+        self.color = color
+
+class Line3D():
+    def __init__(self, start_point, end_point, color='gray', linestyle='solid'):
+        self.start_point = start_point
+        self.end_point = end_point
+        self.color = color
+        self.linestyle = linestyle
+
+class Arrow3D():
+    def __init__(self, head, tail=(0,0,0), color='red', linestyle='solid', head_length=0.2, head_radius=0.08, head_resolution=24):
+        self.head = head
+        self.tail = tail
+        self.color = color
+        self.linestyle = linestyle
+        self.head_length = head_length
+        self.head_radius = head_radius
+        self.head_resolution = head_resolution
+
+def process_pt(pt, color):
+    length = len(pt)
+    x, y, z, col, label = 0, 0, 0, color, None
+    if length > 0:
+        x = pt[0]
+        if length > 1:
+            y = pt[1]
+            if length > 2:
+                z = pt[2]
+                if length > 3:
+                    col = pt[3]
+                    if length > 4:
+                        label = pt[4]
+    return Point3D(x, y, z, color=col, label=label)
+    
+def extract_vectors_3D(objects):
+    for obj in objects:
+        if type(obj) == Point3D:
+            yield (obj.x, obj.y, obj.z)
+        elif type(obj) == Points3D:
+            for p in obj.points:
+                yield (p.x, p.y, p.z)
+        elif type(obj) == Line3D:
+            yield obj.start_point
+            yield obj.end_point
+        elif type(obj) == Arrow3D:
+            yield obj.head
+            yield obj.tail
+        else:
+            raise TypeError("Unrecognized object: {}".format(obj))
+
+def draw_segment(ax, start, end, color="black", linestyle='solid'):
+    all_x, all_y, all_z = [[start[i],end[i]] for i in range(0,3)]
+    ax.plot(all_x, all_y, all_z, color=color, linestyle=linestyle)
+
+def set_translucent_panes(ax, dark_mode, pane_alpha=0.15):
+    if dark_mode:
+        pane_color = 'white'
+        edge_color = 'white'
+    else:
+        pane_color = 'black'
+        edge_color = 'black'
+    
+    rgba = matplotlib.colors.to_rgba(pane_color, pane_alpha)
+    edge_rgba = matplotlib.colors.to_rgba(edge_color, 1)
+
+    # Make the overall axes face transparent so panes aren't masked by it
+    ax.set_facecolor((1, 1, 1, 0))
+
+    for axis in (ax.xaxis, ax.yaxis, ax.zaxis):
+        pane = getattr(axis, "pane", None)
+        if pane is not None:
+            pane.fill = True
+            pane.set_facecolor(rgba)
+            pane.set_edgecolor(edge_rgba)
+            pane.set_alpha(pane_alpha)
+
+def draw3D(*objects, origin=False, axes=True, axes_labels=False, ticks=True, tick_labels=True, grid=False, grid_size=(1,1,1), dark_mode=True, width=6, dpi=100, nice_aspect_ratio=True, save_as=None, azim=None, elev=None, depthshade=True):
+
+    if dark_mode:
+        plt.style.use('dark_background')
+        axes_color = 'white'
+    else:
+        plt.style.use('default')
+        axes_color = 'black'
+
+    fig = plt.figure(figsize=(width, width), dpi=dpi)
+    ax = fig.add_subplot(111, projection='3d')
+    
+    ax.view_init(elev=elev,azim=azim)
+    ax.grid(grid)
+
+    if grid_size:
+        ax.xaxis.set_major_locator(MultipleLocator(grid_size[0]))
+        ax.yaxis.set_major_locator(MultipleLocator(grid_size[1]))
+        ax.zaxis.set_major_locator(MultipleLocator(grid_size[2]))
+    
+    set_translucent_panes(ax, dark_mode=dark_mode)
+    
+    all_vectors = list(extract_vectors_3D(objects))
+    if origin:
+        all_vectors.append((0,0,0))
+    all_x, all_y, all_z = zip(*all_vectors)
+
+    max_x, min_x = max(0,*all_x), min(0,*all_x)
+    max_y, min_y = max(0,*all_y), min(0,*all_y)
+    max_z, min_z = max(0,*all_z), min(0,*all_z)
+
+    x_size = max_x-min_x
+    y_size = max_y-min_y
+    z_size = max_z-min_z
+
+    # Grid
+    rx = max(max_x - min_x, 1e-9)
+    ry = max(max_y - min_y, 1e-9)
+    rz = max(max_z - min_z, 1e-9)
+
+    margin=0.1
+    
+    pad_x = rx * margin
+    pad_y = ry * margin
+    pad_z = rz * margin
+
+    auto_xlim = (min_x - pad_x, max_x + pad_x)
+    auto_ylim = (min_y - pad_y, max_y + pad_y)
+    auto_zlim = (min_z - pad_z, max_z + pad_z)
+
+    ax.set_xlim(auto_xlim)
+    ax.set_ylim(auto_ylim)
+    ax.set_zlim(auto_zlim)
+
+    if axes_labels:
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+
+    x0, x1 = ax.get_xlim()
+    y0, y1 = ax.get_ylim()
+    z0, z1 = ax.get_zlim()
+    
+    if axes:
+        draw_segment(ax, (x0, 0, 0), (x1, 0, 0), color=axes_color)
+        draw_segment(ax, (0, y0, 0), (0, y1, 0), color=axes_color)
+        draw_segment(ax, (0, 0, z0), (0, 0, z1), color=axes_color)
+
+    if origin:
+        ax.scatter([0],[0],[0], color=axes_color, marker='x', depthshade=depthshade)
+
+    # Objects
+    for obj in objects:
+        if type(obj) == Point3D:
+            ax.scatter(obj.x, obj.y, obj.z, color=obj.color, depthshade=depthshade)
+        elif type(obj) == Points3D:
+            all_x = [p.x for p in obj.points]
+            all_y = [p.y for p in obj.points]
+            all_z = [p.z for p in obj.points]
+            all_colors = [p.color for p in obj.points]
+            all_labels = [p.label for p in obj.points]
+            ax.scatter(all_x, all_y, all_z, color=all_colors, depthshade=depthshade)
+        elif type(obj) == Line3D:
+            draw_segment(ax, obj.start_point, obj.end_point, color=obj.color, linestyle=obj.linestyle)
+        elif type(obj) == Arrow3D:
+            head, tail = obj.head, obj.tail
+            length = sqrt((head[0]-tail[0])**2 + (head[1]-tail[1])**2 + (head[2]-tail[2])**2)
+            color, linestyle = obj.color, obj.linestyle
+            head_length, head_radius, head_resolution = obj.head_length, obj.head_radius, obj.head_resolution
+            
+            start = np.asarray(tail, float)
+            d = np.asarray(head, float)
+            n = np.linalg.norm(d)
+            if n == 0:
+                raise ValueError("direction vector must be non-zero")
+            d = d / n
+        
+            shaft_end = start + d * (length - head_length)
+            tip = start + d * length
+        
+            # shaft
+            draw_segment(ax, start, shaft_end, color=color, linestyle=linestyle)
+        
+            # orthonormal basis around d
+            up = np.array([0., 0., 1.]) if abs(d[2]) < 0.999 else np.array([0., 1., 0.])
+            a = np.cross(d, up); a /= np.linalg.norm(a)
+            b = np.cross(d, a)
+        
+            # cone base ring
+            theta = np.linspace(0, 2*np.pi, head_resolution, endpoint=False)
+            base_center = shaft_end
+            ring = [base_center + head_radius*(np.cos(t)*a + np.sin(t)*b) for t in theta]
+        
+            # triangular faces to tip
+            faces = [[ring[i], ring[(i+1)%head_resolution], tip] for i in range(head_resolution)]
+            ax.add_collection3d(Poly3DCollection(faces, color=color))
+        else:
+            raise TypeError("Unrecognized object: {}".format(obj))
+    
+    if not tick_labels:
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.set_zticklabels([])
+    
+    if not ticks:
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_zticks([])
+
+    # Size
+    if nice_aspect_ratio:
+        ax.set_box_aspect([1.0, 1.0, 1.0])  # Equal aspect ratio for 3D
+        fig.set_size_inches(width, width, forward=True)
+    else:
+        ax.set_box_aspect(None)
+        w, h = fig.get_size_inches()
+        scale = width/w
+        fig.set_size_inches(width, h * scale, forward=True)
+        fig.set_dpi(dpi)
+    
+    if save_as:
+        plt.savefig(save_as, dpi=fig.dpi)
+
+    plt.show()
+
+
